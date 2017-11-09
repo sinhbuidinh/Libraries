32ed1d6a86b7644b2db92a1c8a57237484e041e1
Sinh Bui Dinh <sinh.buidinh@gmail.com>
2017-11-09 14:10:11 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+require_once libraries.php
+
+$string = '<font color="#92d050">テ</font><font color="#ff0033">キ</font><font color="#000000">ス</font><font color="#4f81bd">ト</font>';
+
+echo "\n\n\n";
+echo html_cut($string, 2);
